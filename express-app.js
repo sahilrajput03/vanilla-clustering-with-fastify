4a4aeefae81970b79d3ec6fa141e0803403bc2ef
Sahil Rajput <sahilrajput03@gmail.com>
2022-02-06 21:09:11 +0530

    Update express app.

--- express-app.js
+++ express-app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 8080
 
+const port = +process.argv[2] || 8080
+if (typeof port !== 'number') throw "Please provide a valid port number"
+
+let count = 0
 app.get('/', (req, res) => {
+	count++
+	console.log('got here', count)
   res.send('Hello World from Express app!')
 })
 
