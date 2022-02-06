7629b931fbcae6e8c7d14255cf4efc42ce96483c
Sahil Rajput <sahilrajput03@gmail.com>
2022-02-06 19:38:00 +0530

    Update file names.

--- fastify-app.js
+++ fastify-app.js
@@ -3,7 +3,7 @@ const port = 8123;
 
 fastify.get('*', (req, res) => {
   // console.log('yoyoy');
-  res.send('Hello World');
+  res.send('Hello World from Fastify app!');
 });
 fastify.listen(port, () => {
   console.log(`Fastify "Hello World" listening on port ${port}, PID: ${process.pid}`);
